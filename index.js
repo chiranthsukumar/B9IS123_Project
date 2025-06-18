be0ac1cc7f9b925312cd5bad50bb29bd110ec61c
chiranthsukumar <146804913+chiranthsukumar@users.noreply.github.com>
2025-06-18 10:56:50 +0000

    Sample service API using express

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import bodyParser from ‘body-parser
+import bodyParser from 'body-parser';
+import route from './routes/service.js'
 
 const app = express();
 const PORT = 5000
 
 app.use(bodyParser.json());
+app.use('/service', route);
 
 app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`)); 
---
+++ routes/service.js
@@ -0,0 +1,36 @@
+import express from 'express';
+
+const route = express.Router();
+
+const services = [
+    {
+        "id": 1,
+        "name": "Exterior Wash",
+        "price": 500
+ 
+    },
+    {
+        "id": 2,
+        "name": "Body Work",
+        "price": 2000
+ 
+    },
+    {
+        "id": 3,
+        "name": "Maintanence",
+        "price": 1500
+ 
+    },
+    {
+        "id": 4,
+        "name": "Custom works",
+        "price": 3000
+ 
+    },
+]
+
+route.get('/', (req, res) =>{
+    res.send(services);
+});
+
+export default route
