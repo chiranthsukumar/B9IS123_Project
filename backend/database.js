f7fbf7b20417bb36299663a308f1506f396cd34a
Chiranth C <chiranthsukumar02@gmail.com>
2025-07-14 01:28:46 +0100

    Database error fix

--- backend/database.js
+++ backend/database.js
@@ -1,13 +1,14 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.join(__dirname, 'mydata.db');
+const dbPath = path.join(__dirname, 'garage.db');
 const db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
         console.error('Error while opening databse:', err.message);
     }
     else{
         console.log('Connnected to database');
+        initializeTables();
     }
 });
 
